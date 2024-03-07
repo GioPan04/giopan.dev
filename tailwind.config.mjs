5177d6f625ced52d188e7c38a84fb139e2900f67
GioPan04 <gioele@pannetto.com>
2024-03-07 11:14:51 +0100

    feat: basic hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pixel: ['"Press Start 2P"', "system-ui"],
+      },
+      colors: {
+        primary: "#0D1821",
+        secondary: "#E03616",
+        accent: "#008148",
+      },
+    },
+  },
+  plugins: [],
+};
