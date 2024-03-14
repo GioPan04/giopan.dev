b984a0f2c4b9150ac13d204100d2aed70d42a4e6
Gioele Pannetto <gioele@pannetto.com>
2024-03-14 20:17:18 +0100

    feat: basic i18n support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: "https://giopan.dev"
+  site: "https://giopan.dev",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "it"],
+    fallback: {
+      it: "en",
+    },
+  }
 });
