1878a228d5ffb1ec9610986c4c20e9fdf65e80a9
Gioele Pannetto <gioele@pannetto.com>
2024-03-24 19:06:56 +0100

    feat: added support for code highlighting in blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
+import rehypeHighlight from "rehype-highlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap()],
-  site: "https://giopan.dev"
+  site: "https://giopan.dev",
+  markdown: {
+    rehypePlugins: [rehypeHighlight]
+  }
 });
